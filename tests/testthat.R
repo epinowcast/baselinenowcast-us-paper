6be63c6fe9cba54791e2389fee1d010c78d645b6
Kaitlyn Johnson <kaitlyn.johnson@lshtm.ac.uk>
2025-09-17 13:30:27 +0100

    fix typos

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,7 +1,7 @@
 library(testthat)
 library(baselinenowcastpaper)
 
-test_results <- test_check("baselinenowcastpaper")
+test_results <- test_check("baselinenowcast-us-paper")
 
 if (any(as.data.frame(test_results)$warning > 0)) {
   stop("tests failed with warnings", call. = FALSE)
