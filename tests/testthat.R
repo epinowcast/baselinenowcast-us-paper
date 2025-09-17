8b2a03625dcdb267c5848d63a5dda14e2a7f4f64
Kaitlyn Johnson <kaitlyn.johnson@lshtm.ac.uk>
2025-09-17 13:48:37 +0100

    again

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,7 +1,7 @@
 library(testthat)
 library(baselinenowcastuspaper)
 
-test_results <- test_check("baselinenowcast-us-paper")
+test_results <- test_check("baselinenowcastuspaper")
 
 if (any(as.data.frame(test_results)$warning > 0)) {
   stop("tests failed with warnings", call. = FALSE)
